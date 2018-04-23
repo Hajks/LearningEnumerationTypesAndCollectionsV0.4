45ae2353c396e9d04c94c622134b2f7c99e6fad1
Hajk <tunczorx@gmail.com>
2018-04-23 14:57:33 +0200

    Lists and information about them. Gonna continue in next task

--- LearningEnumerationTypesAndCollectionsV0.4/Form1.cs
+++ LearningEnumerationTypesAndCollectionsV0.4/Form1.cs
@@ -12,9 +12,27 @@ namespace LearningEnumerationTypesAndCollectionsV0._4
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            List<Egg> myCarton = new List<Egg> ();
+            Egg x = new Egg();
+            Egg y = new Egg();
+            Chicken z = new Chicken(); // Chicken inherits from egg, that's why we can add to list our chicken.
+
+            myCarton.Add(x); // That's how we add our object
+            myCarton.Add(z);
+            myCarton.Add(y);
+
+            int theSize = myCarton.Count; //We can check how big our list is.
+
+            bool isIn = myCarton.Contains(x); //We can check if our object is in list.
+
+            int idx = myCarton.IndexOf(y); //We can check what index our object have.
+
+            myCarton.Remove(y); ; //We can remove our object from list.
+
         }
     }
 }
